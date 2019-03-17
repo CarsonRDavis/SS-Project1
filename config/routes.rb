4d8e9565f25ad3dddcf6f25b4d57e342683c9f52
Carson Davis <carsondavis340@gmail.com>
2019-03-17 19:53:54 -0400

    Finished task 1 in 3.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'say/filenames'
+  get 'say/hello'
+  get 'say/goodbye'
+  root 'say#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
