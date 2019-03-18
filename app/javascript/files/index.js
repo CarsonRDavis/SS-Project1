ca357f793a3389e1301723316dc535566a83db67
Carson Davis <carsondavis340@gmail.com>
2019-03-17 21:06:52 -0400

    Last minute fixes

--- app/javascript/files/index.js
+++ app/javascript/files/index.js
@@ -5,5 +5,5 @@ import FileApp from "./components/FileApp";
 document.addEventListener("DOMContentLoaded", () => {
     const files = document.querySelector("#files");
     const data = JSON.parse(files.getAttribute("data"));
-    ReactDOM.render(<FileApp name="Your name" data={data} />, files);
+    ReactDOM.render(<FileApp name="Carson Davis" data={data} />, files);
 });
