d2efdb6edd9678f35cb6c8c745c09fb618d38ee0
Carson Davis <carsondavis340@gmail.com>
2019-03-17 20:03:58 -0400

    Finished task 3 in 3.5

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,12 +1,19 @@
 class SayController < ApplicationController
+  before_action :time_now
+
   def hello
-    @time = Time.now
   end
 
   def goodbye
   end
 
   def filenames
-    @files = Dir.glob('*')
+      @files = Dir.glob('*')
+  end
+
+  private
+
+  def time_now
+      @time = Time.now
   end
 end
